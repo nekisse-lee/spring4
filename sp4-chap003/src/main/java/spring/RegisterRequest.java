ff7252f9f0e7eec0f3f1b648eb38830f07a53d38
nekisse <lsh891224@gmail.com>
2017-11-07 19:12:07 +0900

    스프링 DI  assembler

---
+++ sp4-chap003/src/main/java/spring/RegisterRequest.java
@@ -0,0 +1,48 @@
+package spring;
+
+public class RegisterRequest {
+
+	private String email;
+	private String passwod;
+	private String confirmPassword;
+	private String name;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPasswod() {
+		return passwod;
+	}
+
+	public void setPasswod(String passwod) {
+		this.passwod = passwod;
+	}
+
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+	public boolean isPasswordEqualToConfirmPassword() {
+		return passwod.equals(confirmPassword);
+	}
+	
+	
+}
