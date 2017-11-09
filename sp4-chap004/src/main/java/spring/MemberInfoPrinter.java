a9168dcf426e58958d227f51a4a90dd8b7b8a3b0
nekisse <lsh891224@gmail.com>
2017-11-09 18:45:36 +0900

    @Resource  적용순서 1. name 속성의 빈 객체를 찾는다. 존재하면 객체를 주입할 객체로 사용 2. name 속성이 없을경우 동일한 객체 타입을 갖는 빈 객체를 찾고 존재하면 객체를 주입할 객체로 사용. 3. name 속성이 없고 동일한 타입의 빈 객체가 두 개 이상일경우, 같은 이름을 가진 빈 객체를 찾는다.	존재하면 객체를 주입할 객체로 사용 4. name속성이 없고 동일한 타입 빈 객체가 두 개 이상, 같은 이름을 가진 빈 객체가 없을경우 @Qualifier을 이용해서 주입할 빈 객체를 찾는다
    @Autowired 와 				@Resource 차이점
       객체타입을 사용      				빈 네임을 사용
    생성자,필드, 메서드 사용가능		필드, 메서드만 사용가능

--- sp4-chap004/src/main/java/spring/MemberInfoPrinter.java
+++ sp4-chap004/src/main/java/spring/MemberInfoPrinter.java
@@ -1,11 +1,16 @@
 package spring;
 
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
 public class MemberInfoPrinter {
 	
-	@Autowired(required=false)
+//	@Autowired(required=false)  객체를 이용해서 사용
+	
+	
+	@Resource(name="memberDao") // 빈 이름을 이용해서 사용
 	private MemberDao memDao;
 	private MemberPrinter printer;
 
@@ -14,11 +19,17 @@ public class MemberInfoPrinter {
 	}
 
 	
-	@Autowired
-	@Qualifier("sysout")
-	public void setPrinter(MemberDao memberDao,
-			@Qualifier("sysout")MemberPrinter printer) {
-		this.memDao = memberDao;
+//	@Autowired
+//	@Qualifier("sysout")    객체를 이용해서 사용   여러개의 파라미터 받을시 파라미터 에 @Qualifier()   
+//	public void setPrinter(MemberDao memberDao,
+//			@Qualifier("sysout")MemberPrinter printer) {
+//		this.memDao = memberDao;
+//		this.printer = printer;
+//	}
+	
+	
+	@Resource(name="memberPrinter")  //빈 이름을 이용해서 사용!!
+	public void setPrinter(MemberPrinter printer) {
 		this.printer = printer;
 	}
 
--- sp4-chap004/src/main/java/spring/MemberRegisterService.java
+++ sp4-chap004/src/main/java/spring/MemberRegisterService.java
@@ -1,15 +1,18 @@
 package spring;
 
-import static org.junit.Assert.assertFalse;
+//import static org.junit.Assert.assertFalse;
 
 import java.util.Date;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.annotation.Resource;
+
+//import org.springframework.beans.factory.annotation.Autowired;
 
 public class MemberRegisterService {
+	@Resource(name="memberDao")   // 빈이름으로 사용!
 	private MemberDao memberDao;
 
-	@Autowired(required = false)
+//	@Autowired(required = false)   //타입을 이용해서 사용!!
 	public MemberRegisterService(MemberDao memberDao) {
 		this.memberDao = memberDao;
 	}
