bac47a67da76ab82e83ca4c9b2790edb1c8a133f
nekisse <lsh891224@gmail.com>
2017-11-10 19:45:07 +0900

    자바설정을 사용할경우   생성자에 @Autowired  어노테이션을 적용해도 자동주입이 안된다
    필드나 메서드에만 적용가능!

--- sp4-chap005/src/main/java/spring/MemberInfoPrinter.java
+++ sp4-chap005/src/main/java/spring/MemberInfoPrinter.java
@@ -1,22 +1,23 @@
 package spring;
 
-import javax.annotation.Resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 
 public class MemberInfoPrinter {
 
 	private MemberDao memDao;
 	private MemberPrinter printer;
 
+	@Autowired
 	public void setMemberDao(MemberDao memberDao) {
-		System.out.println("MemberInfoPrinter 주입: " + memberDao);
+		// System.out.println("MemberInfoPrinter 주입: " + memberDao);
 		this.memDao = memberDao;
 	}
 
+	
+	@Autowired
 	public void setPrinter(MemberPrinter printer) {
-		System.out.println("setPrinter: " + printer);
+		// System.out.println("setPrinter: " + printer);
 		this.printer = printer;
 	}
 
